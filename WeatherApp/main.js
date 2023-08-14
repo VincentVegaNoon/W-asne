d922bb3ea893a6e603b3d778315c085811fe8705
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-08-14 20:24:38 +0200

    fix

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,10 +1,107 @@
-const h1 = document.querySelector("h1");
-const h2 = document.querySelector("h2");
-const h3 = document.querySelector("h3");
+// const h1 = document.querySelector("h1");
+// const h2 = document.querySelector("h2");
+// const h3 = document.querySelector("h3");
+// const header = document.querySelector("header");
+// const daySection = document.querySelectorAll(".day");
+// const weatherApi =
+//   "https://api.open-meteo.com/v1/forecast?latitude=52.2298&longitude=21.0118&hourly=temperature_2m,rain,is_day&daily=weathercode,temperature_2m_max,apparent_temperature_max&current_weather=true&timezone=Pacific%2FAuckland";
+// const days = {
+//   0: "Sunday",
+//   1: "Monday",
+//   2: "Tuesday",
+//   3: "Wednesday",
+//   4: "Thursday",
+//   5: "Friday",
+//   6: "Saturday",
+// };
+
+// const timeFunc = (number = 0) => {
+//   const now = new Date();
+//   let day = days[now.getDay() + number];
+//   return day;
+// };
+// async function fetchWeatherInfo() {
+//   try {
+//     const response = await fetch(weatherApi);
+//     const data = await response.json();
+//     console.log(data.current_weather);
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// }
+
+// fetchWeatherInfo().then((data) => {
+//   weather(data);
+//   if (data.current_weather.is_day === 0) {
+//     const newStyle = `
+//     .wrapper::before {background: linear-gradient(#10296e7a, #050c6e77);}`;
+//     const styleElement = document.createElement("style");
+//     styleElement.appendChild(document.createTextNode(newStyle));
+//     document.head.appendChild(styleElement);
+//     header.style.color = "white";
+//   } else if (data.current_weather === 1) {
+//     styleElement.remove();
+//     header.style.color = "black";
+//   }
+//   let h1String = data.timezone.replace(/.*\//, "");
+//   h1.textContent = `${h1String}`;
+//   h2.textContent = `${data.current_weather.temperature}${data.hourly_units.temperature_2m}`;
+//   h3.textContent = `${weatherCodes[data.current_weather.weathercode]}`;
+//   for (let i = 1; i <= daySection.length; i++) {
+//     daySection[i - 1].childNodes[1].textContent = timeFunc(i);
+//     daySection[
+//       i - 1
+//     ].childNodes[3].children[0].textContent = `${data.daily.temperature_2m_max[i]} ${data.daily_units.temperature_2m_max}`;
+//     daySection[i - 1].childNodes[3].children[1].textContent =
+//       weatherCodes[data.daily.weathercode[i]];
+//   }
+// });
+
+// function weather(data) {
+//   console.log(data.hourly_units.temperature_2m);
+//   console.log(data);
+// }
+
+// const weatherCodes = {
+//   0: "Clear sky",
+//   1: "Mainly clear",
+//   2: "Partly cloudy",
+//   3: "Overcast",
+//   45: "Fog and depositing rime fog",
+//   48: "Fog and depositing rime fog",
+//   51: "Drizzle: Light intensity",
+//   53: "Drizzle: Moderate intensity",
+//   55: "Drizzle: Dense intensity",
+//   56: "Freezing Drizzle: Light intensity",
+//   57: "Freezing Drizzle: Dense intensity",
+//   61: "Rain: Slight intensity",
+//   63: "Rain: Moderate intensity",
+//   65: "Rain: Heavy intensity",
+//   66: "Freezing Rain: Light intensity",
+//   67: "Freezing Rain: Heavy intensity",
+//   71: "Snow fall: Slight intensity",
+//   73: "Snow fall: Moderate intensity",
+//   75: "Snow fall: Heavy intensity",
+//   77: "Snow grains",
+//   80: "Rain showers: Slight intensity",
+//   81: "Rain showers: Moderate intensity",
+//   82: "Rain showers: Violent intensity",
+//   85: "Snow showers: Slight intensity",
+//   86: "Snow showers: Heavy intensity",
+//   95: "Thunderstorm: Slight or moderate",
+//   96: "Thunderstorm with slight hail",
+//   99: "Thunderstorm with heavy hail",
+// };
+
 const header = document.querySelector("header");
 const daySection = document.querySelectorAll(".day");
-const weatherApi =
-  "https://api.open-meteo.com/v1/forecast?latitude=52.2298&longitude=21.0118&hourly=temperature_2m,rain,is_day&daily=weathercode,temperature_2m_max,apparent_temperature_max&current_weather=true&timezone=auto";
+const input = document.querySelector("input");
+const apiKey = "9cdbba22380927c06d85179a7490438e";
+const btn = document.querySelector("button");
+const infoSection = document.querySelector(".info");
+infoSection.style.display = "none";
 const days = {
   0: "Sunday",
   1: "Monday",
@@ -17,80 +114,41 @@ const days = {
 
 const timeFunc = (number = 0) => {
   const now = new Date();
-  let day = days[now.getDay() + number];
+  let day = days[(now.getDay() + number) % 7];
   return day;
 };
-async function fetchWeatherInfo() {
+btn.addEventListener("click", async () => {
+  const city = input.value;
+  if (city === "") {
+    return;
+  }
   try {
-    const response = await fetch(weatherApi);
-    const data = await response.json();
-    console.log(data.current_weather);
-    return data;
+    response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+    data = await response.json();
+    if ((data.cod = "404")) {
+      infoSection.style.display = "none";
+    }
+    input.value = "";
   } catch (error) {
     console.error(error);
-    return [];
   }
-}
-
-fetchWeatherInfo().then((data) => {
-  weather(data);
-  if (data.current_weather.is_day === 0) {
-    const newStyle = `
-    .wrapper::before {background: linear-gradient(#10296e7a, #050c6e77);}`;
-    const styleElement = document.createElement("style");
-    styleElement.appendChild(document.createTextNode(newStyle));
-    document.head.appendChild(styleElement);
-    header.style.color = "white";
-  } else if (data.current_weather === 1) {
-    styleElement.remove();
-    header.style.color = "black";
-  }
-  let h1String = data.timezone.replace(/.*\//, "");
-  h1.textContent = `${h1String}`;
-  h2.textContent = `${data.current_weather.temperature}${data.hourly_units.temperature_2m}`;
-  h3.textContent = `${weatherCodes[data.current_weather.weathercode]}`;
-  for (let i = 1; i <= daySection.length; i++) {
-    daySection[i - 1].childNodes[1].textContent = timeFunc(i);
-    daySection[
-      i - 1
-    ].childNodes[3].children[0].textContent = `${data.daily.temperature_2m_max[i]} ${data.daily_units.temperature_2m_max}`;
-    daySection[i - 1].childNodes[3].children[1].textContent =
-      weatherCodes[data.daily.weathercode[i]];
-  }
+  createSeletedCity(data);
 });
 
-function weather(data) {
-  console.log(data.hourly_units.temperature_2m);
-  console.log(data);
-}
-
-const weatherCodes = {
-  0: "Clear sky",
-  1: "Mainly clear",
-  2: "Partly cloudy",
-  3: "Overcast",
-  45: "Fog and depositing rime fog",
-  48: "Fog and depositing rime fog",
-  51: "Drizzle: Light intensity",
-  53: "Drizzle: Moderate intensity",
-  55: "Drizzle: Dense intensity",
-  56: "Freezing Drizzle: Light intensity",
-  57: "Freezing Drizzle: Dense intensity",
-  61: "Rain: Slight intensity",
-  63: "Rain: Moderate intensity",
-  65: "Rain: Heavy intensity",
-  66: "Freezing Rain: Light intensity",
-  67: "Freezing Rain: Heavy intensity",
-  71: "Snow fall: Slight intensity",
-  73: "Snow fall: Moderate intensity",
-  75: "Snow fall: Heavy intensity",
-  77: "Snow grains",
-  80: "Rain showers: Slight intensity",
-  81: "Rain showers: Moderate intensity",
-  82: "Rain showers: Violent intensity",
-  85: "Snow showers: Slight intensity",
-  86: "Snow showers: Heavy intensity",
-  95: "Thunderstorm: Slight or moderate",
-  96: "Thunderstorm with slight hail",
-  99: "Thunderstorm with heavy hail",
+const createSeletedCity = (data) => {
+  const h1 = document.querySelector(".temp.condition h1");
+  const temperature = document.querySelector(".temp p");
+  const weatherCondition = document.querySelector(".weather.condition h1");
+  const windCondition = document.querySelector(".wind.condition h1");
+  h1.textContent = data.name;
+  temperature.textContent = `${data.main.temp} °C`;
+  weatherCondition.textContent = data.weather[0].main;
+  windCondition.textContent = "Wind";
+  document.querySelector(
+    ".weather p"
+  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></img>`;
+  document.querySelector(".wind p").textContent = `${data.wind.speed}Km/h`;
+  infoSection.style.display = "block";
 };
