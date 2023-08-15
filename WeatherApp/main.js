c982b9b2421c3b590dae163d71b029f8b114c47b
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2023-08-15 20:20:01 +0200

    fix

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,32 +1,44 @@
 const header = document.querySelector("header");
 const daySection = document.querySelectorAll(".day");
 const input = document.querySelector("input");
-const apiKey = "9cdbba22380927c06d85179a7490438e";
+// const apiKey = "9cdbba22380927c06d85179a7490438e";
 const btn = document.querySelector("button");
 const infoSection = document.querySelector(".info");
+const ul = document.querySelector(".menu__box");
 infoSection.style.display = "none";
+const storedHistory = JSON.parse(localStorage.getItem("history")) || [];
+const fragment = document.createDocumentFragment();
+const hamburgerMenu = document.querySelector(".menu__box");
+btn.addEventListener("click", apiCall);
 
-btn.addEventListener("click", async () => {
+function apiCall() {
   const city = input.value;
+  fetchWeatherData(city);
+  addToHistory(city);
+}
+
+function fetchWeatherData(city) {
+  const apiKey = "9cdbba22380927c06d85179a7490438e";
   if (city === "") {
     return;
   }
-  try {
-    response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    );
-    data = await response.json();
-    if ((data.cod = "404")) {
-      infoSection.style.display = "none";
-    }
-    input.value = "";
-  } catch (error) {
-    console.error(error);
-  }
-  createSeletedCity(data);
-});
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cod === "404") {
+        infoSection.style.display = "none";
+      }
+      input.value = "";
+      createSelectedCity(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
-const createSeletedCity = (data) => {
+const createSelectedCity = (data) => {
   const h1 = document.querySelector(".temp.condition h1");
   const temperature = document.querySelector(".temp p");
   const weatherCondition = document.querySelector(".weather.condition h1");
@@ -41,3 +53,70 @@ const createSeletedCity = (data) => {
   document.querySelector(".wind p").textContent = `${data.wind.speed}Km/h`;
   infoSection.style.display = "block";
 };
+
+const addToHistory = (city) => {
+  if (city.length < 1) {
+    return;
+  }
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+  a.classList.add("menu__item");
+  a.href = "#";
+  a.textContent = city.toUpperCase();
+  ul.prepend(li);
+  li.appendChild(a);
+  if ([...document.querySelectorAll("li")].length > 5) {
+    ul.removeChild(ul.lastChild);
+  }
+  const link = document.querySelectorAll("a");
+  linklistener(link);
+  historyTab(city);
+};
+
+function historyTab(city) {
+  storedHistory.unshift(city.toUpperCase());
+  if (storedHistory.length > 5) {
+    storedHistory.pop();
+  }
+  localStorage.setItem("history", JSON.stringify(storedHistory));
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (storedHistory.length > 0) {
+    storedHistory.forEach((city) => {
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      a.classList.add("menu__item");
+      a.href = "#";
+      a.textContent = city.toUpperCase();
+      fragment.appendChild(li);
+      li.appendChild(a);
+    });
+    ul.appendChild(fragment);
+    const link = document.querySelectorAll("a");
+    linklistener(link);
+  }
+});
+
+function linklistener(link) {
+  link.forEach((elem) => {
+    elem.addEventListener("click", function () {
+      const city = elem.textContent;
+      fetchWeatherData(city);
+      hamburgerMenu.style.display = "none";
+    });
+  });
+}
+
+document
+  .querySelector(".hamburger-menu button")
+  .addEventListener("click", function () {
+    if (hamburgerMenu.style.display === "block") {
+      hamburgerMenu.style.display = "none";
+    } else {
+      hamburgerMenu.style.display = "block";
+    }
+  });
+input.addEventListener("click", function () {
+  hamburgerMenu.style.display = "none";
+});
